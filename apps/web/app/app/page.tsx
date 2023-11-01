8203d466ce96f16055b03b95310f2f3a54a456e4
chroxify <chvtodorov@gmail.com>
2023-11-01 14:38:16 +0100

    fix: improve no projects screen

--- apps/web/app/app/page.tsx
+++ apps/web/app/app/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation';
-import { Button } from 'ui/components/ui/button';
 import { getUserProjects } from '@/lib/api/user';
-import AddProjectDialog from '@/components/dashboard/modals/add-project-modal';
+import Onboarding from '@/components/layout/onboarding';
 
 export default async function Projects() {
   const { data: projects, error } = await getUserProjects('server');
@@ -22,28 +21,8 @@ export default async function Projects() {
 
   // TODO: Improve this and make this redirect to an onboarding page if the user has no projects
   return (
-    <div className='flex h-full w-full flex-col overflow-y-auto'>
-      {/* Big Title */}
-      {/* <div className='pb-11 text-3xl font-bold'>Projects</div> */}
-
-      {/* Content */}
-      <div className='flex h-full w-full flex-col gap-2'>
-        {/* No Projects */}
-        <div className='flex flex-col items-center justify-center gap-2 pt-32'>
-          <div className='text-2xl font-bold'>No projects yet</div>
-          <div className='text-foreground/60 text-center text-lg'>
-            Create your first project to get started.
-          </div>
-
-          <AddProjectDialog
-            trigger={
-              <Button size='sm' variant='outline' className='mt-2 flex items-center gap-2'>
-                Create first project
-              </Button>
-            }
-          />
-        </div>
-      </div>
+    <div className='flex h-screen w-full items-center justify-center'>
+      <Onboarding />
     </div>
   );
 }
